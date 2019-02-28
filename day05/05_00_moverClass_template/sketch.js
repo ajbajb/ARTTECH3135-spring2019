81f6870da60baadfaa31be77868b2a6edc35e1d0
ajbajb <abach@saic.edu>
2019-02-28 10:20:48 -0600

    mover template

---
+++ day05/05_00_moverClass_template/sketch.js
@@ -0,0 +1,74 @@
+
+'use strict'
+
+let movers = [];
+const numMovers = 1;
+
+function setup() {
+
+    createCanvas( 600, 600 );
+
+    for ( let i = 0; i < numMovers; i++ ) {
+
+        createMover();
+
+    }
+
+}
+
+function draw() {
+
+    background( 0 );
+
+    for ( let i = 0; i < movers.length; i++ ) {
+
+        movers[i].update();
+        movers[i].display();
+
+    }
+
+}
+
+function createMover() {
+
+    let mover = new Mover();
+    mover.position = createVector( 100, 100 );
+    mover.mass = random( 0.1, 1.0 );
+    mover.col = color( random( 255 ), random( 255 ), random( 255 ) );
+
+    movers.push( mover );
+
+    console.log( mover );
+
+}
+
+function Mover() {
+
+    this.position = createVector( 0, 0 );
+    this.velocity = createVector( 0, 0 );
+    this.acceleration = createVector( 0, 0 );
+
+    this.mass = 0.5;
+    this.size = function() {
+        return this.mass * 10;
+    }
+    this.topSpeed = 20;
+    this.col = color( 255, 200, 0 );
+
+    this.update = function() {
+
+        this.velocity.add( this.acceleration );
+        this.velocity.limit( this.topSpeed );
+        this.position.add( this.velocity );
+
+    }
+
+    this.display = function() {
+
+        noStroke();
+        fill( this.col );
+        ellipse( this.position.x, this.position.y, this.size(), this.size() );
+
+    }
+
+}
