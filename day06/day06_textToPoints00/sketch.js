a102ce28dffeb229ecda603d1b42ef5d1c210c63
ajbajb <abach@saic.edu>
2019-03-11 10:27:50 -0500

    textToPoints example

--- day06/day06_textToPoints00/sketch.js
+++ day06/day06_textToPoints00/sketch.js
@@ -1,24 +1,39 @@
 
-
-let fontFile = ' ';
+'use strict'
+// path to out font file
+let fontFile = 'assets/AlexBrush-Regular.ttf';
+let font;
+// an array to hold the points extracted from textToPoints();
+let points = [];
 
 function preload() {
 
-    font = loadFont( fontFile );
+    // load the font
+    font = loadFont( fontFile, function() {
+        console.log( "Font loaded" );
+    } );
 
 }
 
 function setup() {
 
-    creatCanvas( 600, 600 );
+    createCanvas( 600, 600 );
 
-    // textToPonts
-    let points = textToPoints( textStr, width / 2, height / 2, 60, {
+    let textStr = "BLOT";
 
-        sampleFactor: 1,
+    // run textToPoints on the font and put the reult in points[]
+    //
+    // pass in the text to extract the points from
+    // the tets location on the canvas, and its size
+    // think of the sampleFactor as the 'resolution', lower number will reult in less points
+    points = font.textToPoints( textStr, width / 4, height / 2, 90, {
+
+        sampleFactor: 0.5,
         simplifyThreshold: 0
 
     } );
+
+    console.log( points );
 
 }
 
@@ -26,17 +41,17 @@ function draw() {
 
     background( 0 );
 
+    let time = millis() * 0.0005;
+
+    // iterate through points array and draw the points
+    for ( let i = 0; i < points.length; i++ ) {
+
+        let offset = sin( time + ( -i * 0.0009 ) ) * 25 + 25;
+        let p = points[i];
+        noStroke();
+        fill( 255, 0, 255, p.alpha );
+        ellipse( p.x, p.y, offset, offset );
+
+    }
 
 }
-
-
-
-
-
-// fill( words[0].r, words[0].g, words[0].b );
-// textSize( words[0].size );
-// text( words[0].word, 100, 100 );
-//
-// fill( words[1].r, words[1].g, words[1].b );
-// textSize( words[1].size );
-// text( words[1].word, 200, 200 );
